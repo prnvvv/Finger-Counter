f23e3295123962d81ae0df67a5f014c453401c7c
prnvvv <prannavakhanth12@gmail.com>
2024-06-16 20:33:24 +0530

    Update FingerCounter_project.py

--- FingerCounter_project.py
+++ FingerCounter_project.py
@@ -1,16 +1,26 @@
 import cv2
+import time
 
 capture = cv2.VideoCapture(0)
 
+capture.set(3, 640)
+capture.set(4, 480)
+
 if not capture.isOpened():
     raise Exception("Error: Loading not successful.")
+
+previousTime = currenTime = 0
 
 while True:
     success, vidObject = capture.read()
 
     if not success:
-        break
+        raise Exception("Error: Reading frame was not successful.")
 
+    currenTime = time.time()
+    fps = 1 / (currenTime - previousTime)
+    previousTime = currenTime
+    
     cv2.imshow("Webcamera", vidObject)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
